a7098c8c1c85a748e601f5b3bf996fce713af207
Anel Martéz <anelmartez18@gmail.com>
2024-07-15 17:28:02 -0500

    Inicializar repositorio de backend

---
+++ online_store_backend/store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
